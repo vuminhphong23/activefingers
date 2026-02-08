49663f66a2f52128bf830569362418de155aeae0
PhongVu <phongvuminh2003@gmail.com>
2026-02-09 01:52:41 +0700

    color

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,13 @@
-// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
-import "/public/assets/css/animate.min.css"
-import "/public/assets/css/bootstrap.min.css"
-import "/public/assets/css/flaticon.min.css"
-import "/public/assets/css/fontawesome-5.14.0.min.css"
-import "/public/assets/css/magnific-popup.min.css"
-import "/public/assets/css/nice-select.min.css"
-import "/public/assets/css/slick.min.css"
-import "/public/assets/css/style.css"
+import "../public/assets/css/animate.min.css"
+import "../public/assets/css/bootstrap.min.css"
+import "../public/assets/css/flaticon.min.css"
+import "../public/assets/css/fontawesome-5.14.0.min.css"
+import "../public/assets/css/magnific-popup.min.css"
+import "../public/assets/css/nice-select.min.css"
+import "../public/assets/css/slick.min.css"
+import "../public/assets/css/style.css"
 
 import { Playfair, Plus_Jakarta_Sans } from 'next/font/google'
 
